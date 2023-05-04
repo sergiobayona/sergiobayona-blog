c5533f33afeb0902c7756166ee14812d46010f61
Sergio Bayona <sbayona@martianinteractive.com>
2023-05-04 12:15:45 -0500

    header customization

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <Image src="/profile.png" width={72} height={72} alt="Yours Truly" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Sergio Bayona';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Personal Blog';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
